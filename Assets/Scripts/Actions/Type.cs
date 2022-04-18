1ff741b93fbd721f20e8fe5bd3666f8160b7252e
Alex Silvestri <alexsilvestri.16@gmail.com>
2022-04-18 12:37:57 +0200

    Typing updated

--- Assets/Scripts/Actions/Type.cs
+++ Assets/Scripts/Actions/Type.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 [System.Serializable]
 public class Type
 {
-    [HideInInspector] public enum ActionType { VIOLENCE, PROTECTION, MANIPULATION, MOCKERY, CHARITY, DEBUG };
+    [HideInInspector] public enum ActionType { FIGHT, RESIST, CONVINCE, PLEAD, CONCENTRATE, HELP, ANALYZE, WEAPON, GIFT, JOKE, TRICK };
     public ActionType actionType;
     [HideInInspector] public enum ActionOpinion { SEVERELY_NEGATIVE, NEGATIVE, NEUTRAL, POSITIVE, SEVERELY_POSITIVE };
 }
